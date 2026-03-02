90890bc1785cba0442b1cb9a646e53479f12f624
kentxxq <kentxxq@gmail.com>
2026-03-02 08:56:32 +0800

    feat: 为扩展添加 `host_permissions` 以允许访问所有URL

--- wxt.config.ts
+++ wxt.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     description: '__MSG_extension_description__',
     default_locale: 'zh_CN',
     permissions: ['storage'],
+    host_permissions: ['<all_urls>'],
     action: {
       default_title: '__MSG_extension_name__',
     },
